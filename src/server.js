0589f0bc16a4ea99d79c7836e973a73a853d16da
CG Remuzzi <carlosremuzzi@gmail.com>
2020-05-14 15:55:05 -0400

    requiring path as const

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const fs = require('fs')
 const https = require('https')
 const morgan = require('morgan');
+const path = require('path')
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
